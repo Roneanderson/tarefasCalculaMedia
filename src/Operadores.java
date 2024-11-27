88b0e120472e7686cd5cd5c5802c0dab41a69926
Rone <roneanderson469@gmail.com>
2024-11-27 00:19:46 -0300

    atualizando notas

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,44 +1,18 @@
-public class Operadores {
+public static void main(String[] args) {
+    /** Exercicio 01:
+     * O programa calculara a media do aluno junto com suas notas obtidas
+     */
 
-    public static void main(String[] args) {
-        operacoesSomar();
-        operacoesSubtarir();
-        operacoesDividir();
-        operacoesMultiplicar();
-    }
-    //operação somar
-    private static void operacoesSomar() {
-        System.out.println("**** operação de somar ****");
-        int numero1 = 10;
-        int numero2 = 10;
-        int somar = (numero1 + numero2) / 4;
-        System.out.println("O valor e " + somar);
-    }
+    Scanner informarNotas = new Scanner(System.in); {
+        float nota1, nota2, nota3, nota4, media;
 
-    //operação subtarir
-    private static void operacoesSubtarir() {
-        System.out.println("**** operação de subtração ****");
-        int numero1 = 50;
-        int numero2 = 10;
-        int subtarir = (numero1 - numero2) / 4;
-        System.out.println("O valor e " + subtarir);
-    }
+        nota1 = informarNotas.nextFloat();
+        nota2 = informarNotas.nextFloat();
+        nota3 = informarNotas.nextFloat();
+        nota4 = informarNotas.nextFloat();
 
-    //operação dividir
-    private static void operacoesDividir() {
-        System.out.println("**** operação de divisão ****");
-        int numero1 = 20;
-        int numero2 = 5;
-        int dividir = (numero1 / numero2) / 4;
-        System.out.println("O valor e " + dividir);
-    }
-
-    //operação de multiplicar
-    private static void operacoesMultiplicar() {
-        System.out.println("**** operação de multiplicação ****");
-        int numero1 = 10;
-        int numero2 = 100;
-        int multiplicar = numero1 * numero2 / 4;
-        System.out.println("O valor e " + multiplicar);
+        media = nota1 + nota2 + nota3 + nota4 / 4;
+        System.out.println("A média das notas è: "+media);
     }
 }
+
